472ec0c2661fe1781792cfd147de5e0c24d1a222
ViavaCos <13811092694@163.com>
2021-07-11 16:04:04 +0800

    添加字母索引分类

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="hello">
-    <div class="data-item" v-for="(item, index) in LargeData" :key="index">
-      <p>{{ item.name }}</p>
-      <p>{{ item.age }}</p>
+  <div class="wrap">
+    <input placeholder="输入关键字查询" type="text">
+    <br><br>
+    <div class="hello">
+      <div 
+        class="data-title" 
+        :class="{active: selectLetter===key}" 
+        v-for="(item, key) in tempObject" 
+        :key="key" 
+        @click="letterClick(key)"
+      >
+        <p>{{ key }}</p>
+      </div>
+    </div>
+    <hr>  
+    <div class="hello">
+      <div class="data-item" v-for="(item, index) in resultData" :key="index">
+        <p>{{ item.name }}</p>
+        <p>{{ item.age }}</p>
+        <p>{{ item.letter }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -13,11 +30,70 @@ export default {
   name: 'HelloWorld',
   data(){
     return {
-      LargeData: Object.freeze(LargeData)
+      LargeData: Object.freeze(LargeData),
+      selectLetter: '', // 当前选中的字母
+      searchKeywords: '', // 搜索关键字
+      tempObject: {}
     }
   },
   created(){
-    console.log(this.LargeData);
+    // console.log(this.LargeData);
+    this.updateDataStructure(this.LargeData)
+    // this.initIndexedDB()
+  },
+  computed: {
+    // 结果渲染
+    resultData(){
+      let result = this.LargeData || []
+      if(this.selectLetter) {
+        result = this.tempObject[this.selectLetter]
+      }
+
+      if(this.searchKeywords) {
+        return result.filter(item => item.name.includes(this.searchKeywords))
+      }
+      
+      return result
+    }
+  },
+  methods:{
+    // initIndexedDB(){
+    //   const request = window.indexedDB.open('data');
+    //   let db;
+    //   request.onsuccess = () => {
+    //     console.log('打开数据库成功');
+    //     db = request.result
+    //   }
+
+    //   request.onerror = () => {
+    //     console.log('打开数据库失败');
+    //   }
+
+    //   request.onupgradeneeded = (event) => {
+    //     const db = event.target.result
+    //     const objectStore = db.createObjectStore('info', {
+    //       // keyPath: ''
+    //     })
+    //     objectStore.createIndex('name', 'name', { unique: false })
+    //   }
+    // },
+    // 更新数据结构
+    updateDataStructure(data){
+      const tempObject = {}
+      data.forEach(item => {
+        if(!tempObject[item.letter]) {
+          tempObject[item.letter] = []
+        }
+        tempObject[item.letter].push(item)
+      })
+      console.log(tempObject);
+      this.tempObject = tempObject
+      return tempObject
+    },
+    // 字母点击
+    letterClick(letter){
+      this.selectLetter =  this.selectLetter === letter ? '' : letter
+    }
   }
 }
 </script>
@@ -26,6 +102,18 @@ export default {
 .hello {
   display: flex;
   flex-wrap: wrap;
+}
+.data-title {
+  flex: 1;
+  margin: 2px;
+  cursor: pointer;
+  background-color: lightcoral;
+}
+.data-title:hover{
+  opacity: 0.5;
+}
+.data-title.active{
+  background-color: lightskyblue;
 }
 .data-item {
   width: 105px;
